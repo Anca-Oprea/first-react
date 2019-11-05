ebd9d516bc3d2b462d5ac9cedc1f6e1ace7bebcc
Oprea Anca <opreaanca1987@gmail.com>
2019-11-05 21:15:29 +0100

    Updates

---
+++ precache-manifest.0ad30128f76f8b0f6e485e4720fa6400.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "56ea942e1bc073ebdca8cccb1fafb71e",
+    "url": "/first-react/index.html"
+  },
+  {
+    "revision": "efdcb4a1437e27eee33c",
+    "url": "/first-react/static/css/main.a6c49b6d.chunk.css"
+  },
+  {
+    "revision": "155ef2722dab3841f008",
+    "url": "/first-react/static/js/2.7a718ea0.chunk.js"
+  },
+  {
+    "revision": "efdcb4a1437e27eee33c",
+    "url": "/first-react/static/js/main.d792c044.chunk.js"
+  },
+  {
+    "revision": "ece4679ccb756531191b",
+    "url": "/first-react/static/js/runtime-main.25fe5149.js"
+  }
+]);
